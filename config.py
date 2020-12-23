9979e05bd104a66dfbc3d5b5011ac537d8c8e8c2
kid <darkred424@gmail.com>
2020-12-23 17:38:15 +0800

    add back config

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+START_URL = 'http://www.cdjxjy.com'
+USER_NAME = 'aaaaaaaaaa'
+PASSWORD = '********'
+SUBJECT = u'语文'
+STUDY_URL = 'http://www.cdjxjy.com/student/SelectCourseRecord.aspx'
+
