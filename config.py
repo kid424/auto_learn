ffe40103880d373188764de6ad35bd678b296840
kid <darkred424@gmail.com>
2020-12-23 17:34:19 +0800

    untrack config

--- config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-START_URL = 'https://www.cdjxjy.com'
-USER_NAME = 'aaaaaaaaa'
-PASSWORD = '********'
-SUBJECT = u'语文'
-STUDY_URL = 'https://www.cdjxjy.com/student/SelectCourseRecord.aspx'
-
